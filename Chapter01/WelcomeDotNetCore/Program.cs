26a6c1ec76d4a9236eb6b2d8c192a36f1611c388
Anthony Attard <attaant@bigpond.net.au>
2018-07-27 19:39:28 +1000

    First Commit

---
+++ Chapter01/WelcomeDotNetCore/Program.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// Welcome Dont Net Core
+/// </summary>
+namespace WelcomeDotNetCore
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            System.Console.WriteLine("Wecome, .NET Core!");
+        }
+    }
+}
---
+++ Chapter02/Basics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Basics
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Anthony!");
+            Console.WriteLine("Today is {0:D}", DateTime.Today);
+        }
+    }
+}
